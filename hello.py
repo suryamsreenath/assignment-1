8cd366459a40e7ab84f80d131889a69596db571c
suryam sreenath <suryamsree7@gmail.com>
2023-05-15 06:04:57 +0530

    chainging the program

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print ("hello world")
+#palindrome program 
+a='sun'
+b='dad'
+c='mom'
+
+if a==a[::-1]:
+    print(a+ ' is a palindrome')
+else:
+    print(a+ ' is not a palindrome')
+
+    if b==b[::-1]:
+        print(b+ ' is a palindrome')
+    else:
+        print(b+ 'is not a palindrome')
+    if c==c[::-1]:
+        print(c+ ' is a palindrome')
+    else:
+        print(c+ ' is not a palindrome')
